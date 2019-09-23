a74e680f7b0f6a86038730202a1f79ac992ab1e2
vipeller <viktor.peller@microsoft.com>
2019-09-23 09:59:40 -0700

    Make Consumer Group names parameterizable for E2E test instead of using $Default, to avoid IoT Hub connection limit for the same consumer group in case of parallel tests

--- scripts/linux/runE2ETest.sh
+++ scripts/linux/runE2ETest.sh
@@ -351,6 +351,9 @@ function process_args() {
         elif [ $saveNextArg -eq 29 ]; then
             DPS_MASTER_SYMMETRIC_KEY="$arg"
             saveNextArg=0
+        elif [ $saveNextArg -eq 30 ]; then
+            EVENT_HUB_CONSUMER_GROUP="$arg"
+            saveNextArg=0
         else
             case "$arg" in
                 '-h' | '--help' ) usage;;
@@ -383,6 +386,7 @@ function process_args() {
                 '-installRootCAKeyPassword' ) saveNextArg=27;;
                 '-dpsScopeId' ) saveNextArg=28;;
                 '-dpsMasterSymmetricKey' ) saveNextArg=29;;
+				'-eventHubConsumerGroup' ) saveNextArg=30;;
                 '-cleanAll' ) CLEAN_ALL=1;;
                 * ) usage;;
             esac
@@ -621,6 +625,7 @@ function run_longhaul_test() {
     local device_id="$RELEASE_LABEL-Linux-$image_architecture_label-longhaul"
 
     sed -i -e "s@<Analyzer.DeviceID>@$device_id@g" "$deployment_working_file"
+	sed -i -e "s@<Analyzer.ConsumerGroupId>@${EVENT_HUB_CONSUMER_GROUP:-longhaul}@g" "$deployment_working_file"
 
     test_start_time="$(date '+%Y-%m-%d %H:%M:%S')"
     print_highlighted_message "Run Long Haul test with -d '$device_id' started at $test_start_time"
@@ -699,6 +704,7 @@ function run_stress_test() {
     local device_id="$RELEASE_LABEL-Linux-$image_architecture_label-stress"
 
     sed -i -e "s@<Analyzer.DeviceID>@$device_id@g" "$deployment_working_file"
+	sed -i -e "s@<Analyzer.ConsumerGroupId>@${EVENT_HUB_CONSUMER_GROUP:-stress}@g" "$deployment_working_file"
 
     test_start_time="$(date '+%Y-%m-%d %H:%M:%S')"
     print_highlighted_message "Run Stress test with -d '$device_id' started at $test_start_time"
@@ -944,6 +950,7 @@ function usage() {
     echo ' -containerRegistryPassword      Password of given username for container registory.'
     echo ' -iotHubConnectionString         IoT hub connection string for creating edge device.'
     echo ' -eventHubConnectionString       Event hub connection string for receive D2C messages.'
+	echo ' -eventHubConsumerGroup          An existing consumer group id for D2C messages.'
     echo ' -loadGenMessageFrequency        Frequency to send messages in LoadGen module for long haul and stress test. Default is 00.00.01 for long haul and 00:00:00.03 for stress test.'
     echo ' -snitchAlertUrl                 Alert Url pointing to Azure Logic App for email preparation and sending for long haul and stress test.'
     echo ' -snitchBuildNumber              Build number for snitcher docker image for long haul and stress test. Default is 1.1.'
