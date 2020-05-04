2898dfe22711d532f12909c3098d3ff636894bf3
vipeller <viktor.peller@microsoft.com>
2020-05-04 16:30:48 -0700

    code review feedbacks
     interactive rebase in progress; onto adabc5ae
     Last commands done (9 commands done):
        pick 601e2ee9 removed unused static method
        pick 40ed66d5 code review feedbacks
     Next commands to do (9 remaining commands):
        pick 9e593c17 removed 'public' from internally used static method
        pick 26fddfe8 made guard object readonly
     You are currently rebasing branch 'vipeller/eh_bridge' on 'adabc5ae'.
    
     Changes to be committed:
    	deleted:    edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/identity/IUsernameParser.cs
    	deleted:    edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/identity/MqttUsernameParser.cs
    	modified:   edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.MqttBrokerAdapter/AuthAgentRequestHandler.cs
    	renamed:    edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Core.Test/UsernameParserTest.cs -> edge-hub/test/Microsoft.Azure.Devices.Edge.Hub.Mqtt.Test/UsernameParserTest.cs

--- edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Core/identity/IUsernameParser.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-namespace Microsoft.Azure.Devices.Edge.Hub.Core.Identity
-{
-    public interface IUsernameParser
-    {
-        (string deviceId, string moduleId, string deviceClientType) Parse(string username);
-    }
-}
